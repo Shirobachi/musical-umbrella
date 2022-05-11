a322f72e1d3701beaadfc0918cd0f4ac3ea008af
Simon Hryszko <simon@hryszko.dev>
2022-05-11 18:43:56 +0200

    Init frontend with pinia

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+	import { useCounterStore } from '../stores/token'
+
+			const counter = useCounterStore()
+
+			counter.count++
+			// with autocompletion ✨
+			counter.$patch({ count: counter.count + 1 })
+			// or using an action instead
+		counter.increment()
+</script>
+
+<template>
+	<div>
+		Test: {{counter.count}}
+		<!-- button -->
+		<button @click="counter.increment">Increment</button>
+	</div>
+</template>
