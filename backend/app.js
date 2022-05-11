9d830cdb86b27f6269e49bd2b16284d762e33af6
Simon Hryszko <simon@hryszko.dev>
2022-05-11 19:12:09 +0200

    Added route

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,11 @@ const port = 3001
 
 const common = require('./common')
 
+// ROUTES
+const auth = require('./routers/auth');
+app.use('/auth', auth);
 
+// START
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
