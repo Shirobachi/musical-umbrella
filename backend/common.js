4bfab18c6cf16b868147fd0990495b6a1100b9a4
Simon Hryszko <simon@hryszko.dev>
2022-05-11 22:15:16 +0200

    Made function for connection to DB

--- backend/common.js
+++ backend/common.js
@@ -1,7 +1,21 @@
-function test(){
-	return "test";
-}
+// .env file
+DB_LOGIN = process.env.DB_LOGIN
+DB_PASS = process.env.DB_PASS
+DEBUG = process.env.DEBUG || false
+DB_DATABASE = process.env.DB_DATABASE + DEBUG ? '-dev' : ''
 
+
+// config mongo DB
+async function connectDB (collectionName) {
+	const {MongoClient}=require('mongodb');
+	const URI = `mongodb+srv://${DB_LOGIN}:${DB_PASS}@api-hryszko-dev.eqopn.mongodb.net?authSource=admin&replicaSet=atlas-xgft2n-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true`
+	const client = await new MongoClient(URI); 
+	await client.connect();
+
+	return client.db(DB_DATABASE).collection(collectionName);
+}	
+
+// export
 module.exports = {
-	test: test()
+	connectDB,
 }
