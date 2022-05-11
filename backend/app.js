501ed5ad81f9ef566582d27800c3fa7cff6a31d3
Simon Hryszko <simon@hryszko.dev>
2022-05-11 22:14:35 +0200

    refactor

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3001
+require('dotenv').config(); // for environment variables
 
-const common = require('./common')
+const express = require('express') // Import express
+var app = express() // Create an instance of express
+const bp = require('body-parser') // Import body-parser
+const port = process.env.PORT || 3001 // Set port
+
+// For body purpose
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
 
 // ROUTES
 const auth = require('./routers/auth');
