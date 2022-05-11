80195758016ee3c76f28cb952b920bb9d979b437
Simon Hryszko <simon@hryszko.dev>
2022-05-11 19:12:05 +0200

    Added common file

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const common = require('./common')
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
