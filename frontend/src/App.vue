c0d1a214904cc96d28a03a6b108721897cef94d0
Simon Hryszko <simon@hryszko.dev>
2022-05-12 15:11:55 +0200

    changed menu item obj generatiing (now coputed)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,53 @@
 <script setup>
 	import Menu from './components/Menu.vue';
 	import Footer from './components/Footer.vue';
+	import { useTokenStore } from './stores/token'
+	import { computed } from 'vue';
 
-	const menuItems = {
-		mainMenu: [
-			{
-				name: 'Home',
-				link: '/',
-			},
-			{
-				name: 'Something',
-				link: 'sadas',
-			},
-		], 
-		optionalManu: [
-			{
-				name: 'Sign in',
-				link: '/sign-in',
-			},
-			{
-				name: 'Sign up',
-				link: '/sign-up',
-			},
-		]
-	}
+	const token = useTokenStore()
+
+	const menuItems = computed(() => {
+		var temp = {
+			mainMenu: [
+				{
+					name: 'Home',
+					link: '/',
+				},
+				{
+					name: 'Something',
+					link: 'sadas',
+				},
+			], 
+			optionalMenu: []
+		}
+
+		console.log("🚀 ~ file: App.vue ~ line 25 ~ menuItems ~ token.token", token.token? "true": "false");
+		if (!token.token) {
+			temp.optionalMenu = [
+				{
+					name: 'Sign up',
+					link: '/sign-up',
+				},
+				{
+					name: 'Sign in',
+					link: '/sign-in',
+				},
+			]
+		}
+		else{
+			temp.optionalMenu = [
+				{
+					name: 'Sign out',
+					link: '/sign-out',
+				},
+			]
+		}
+
+		return temp;
+	});
+
+
+	
 </script>
 
 <template>
