a5df3fda78e174accbd8b3d98cd1c93bcbccffb7
Simon Hryszko <simon@hryszko.dev>
2022-05-12 15:12:38 +0200

    added sign out view

---
+++ frontend/src/views/Sign-out.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+	import { computed, ref, onMounted } from 'vue';
+  import { useToast } from "vue-toastification";
+  import axios from "axios";
+	import { useTokenStore } from '../stores/token'
+	import router from '../router'
+
+	const Toast = useToast();
+
+	onMounted(async () => {
+		const token = await useTokenStore()
+		token.clearToken();
+
+		// redirect to home page
+		router.push('/');
+	})
+</script>
+
+<template>
+
+	<div class="text-center">
+
+		Loggin out...
+
+	</div>
+
+</template>
