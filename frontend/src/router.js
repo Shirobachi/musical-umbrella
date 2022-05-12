3db2764c2cb21abedb3ebc93e116007409d4310c
Simon Hryszko <simon@hryszko.dev>
2022-05-12 14:44:25 +0200

    added sing in view

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
   { name: "Home", path: "/", component: () => import("./views/Home.vue") },
   { name: "Sign-up", path: "/sign-up", component: () => import("./views/Sign-up.vue") },
+  { name: "Sign-in", path: "/sign-in", component: () => import("./views/Sign-in.vue") },
 ];
 
 const router = createRouter({
--- frontend/src/views/Sign-in.vue
+++ frontend/src/views/Sign-in.vue
@@ -3,37 +3,38 @@
 	import { computed, ref } from 'vue';
   import { useToast } from "vue-toastification";
   import axios from "axios";
+	import { useTokenStore } from '../stores/token'
 
+	const token = useTokenStore()
 	const Toast = useToast();
 	const base_URI = import.meta.env.VITE_BASE_BACKEND_ENDPOINT;
 
 	const user = ref({
-		name: '',
-		email: '',
-		password: '',
-		passwordConfirmation: '',
+		name: 'dasadsads',
+		password: '2WxU0Gkx2q8VbcTPuNrJHonPuQn9yGLmIdvaMKfNuwD3ZwX3',
 	})
 
 	const submit = () => {
-		if(user.value.password !== user.value.passwordConfirmation) {
-			return Toast.error('Passwords do not match');
+		if (user.value.password == "" || user.value.name == "") {
+			return Toast.error('Please fill in all the fields');
 		}
 
-		console.log(`${base_URI}/auth`)
-
 		axios({
-			method: 'POST',
+			method: 'GET',
 			url: `${base_URI}/auth`,
-			data: user.value,
-		}).then(() => {
-			Toast.success('Successfully signed up, you can now log in!');
-			// redirect to login
+			headers: {
+				Authorization: `Basic ${btoa(user.value.name + ":" + user.value.password)}`
+			} 
+		}).then((r) => {
+			Toast.success('Successfully logged in!');	
+			token.setToken(r.data.token);
 		}).catch((e) => {
 			if (e.response.data)
 				Toast.error( e.response.data.message || "We struggling with goblins on out server 👺" );
 			else
 				Toast.error( "We struggling with goblins on out server 👺" );
 		});
+
 	}
 </script>
 
@@ -46,30 +47,24 @@
 				<div class="flex flex-col space-y-2 items-center justify-center">
 					<input class="w-11/12 focus:w-full bg-gray-600 shadow appearance-none border rounded py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline"
 						type="text"
-						placeholder="name"
+						placeholder="name or e-mail"
 						v-model="user.name">
-					<input class="w-11/12 focus:w-full bg-gray-600 shadow appearance-none border rounded py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline"
-						type="text"
-						placeholder="E-mail"
-						v-model="user.email">
 					<input class="w-11/12 focus:w-full bg-gray-600 shadow appearance-none border rounded py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline"
 						type="password"
 						placeholder="Password"
 						v-model="user.password">
-					<input class="w-11/12 focus:w-full bg-gray-600 shadow appearance-none border rounded py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline"
-						type="password"
-						placeholder="Repeat password"
-						v-model="user.passwordConfirmation">
 				</div>
 			</div>
 
 			<!-- SUBMIT -->
 			<div class="flex items-center justify-center">
 				<button @click="submit" class="bg-blue-500 hover:bg-blue-700 text-dark font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-					Sign up
+					Sign In
 				</button>
 			</div>
 		</form>
+
+	{{token.token}}
 	</div>
 
 
