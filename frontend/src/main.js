b99a1e1bc74d5d32681747c3738433b667a4c393
Simon Hryszko <simon@hryszko.dev>
2022-05-12 13:30:06 +0200

    Added axios, router and toast libs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,16 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import './index.css'
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
+app.use(Toast, {
+	position: "bottom-right",
+	timeout: 2000,
+})
 app.mount('#app')
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   { name: "Home", path: "/", component: () => import("./views/Home.vue") },
+  { name: "Sign-up", path: "/sign-up", component: () => import("./views/Sign-up.vue") },
 ];
 
 const router = createRouter({
