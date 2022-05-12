84db87e24d94bb2259989b8022491bbe3ba86cf9
Simon Hryszko <simon@hryszko.dev>
2022-05-12 13:30:31 +0200

    Added signup view

---
+++ frontend/src/views/Sign-up.vue
@@ -0,0 +1,76 @@
+<script setup>
+
+	import { computed, ref } from 'vue';
+  import { useToast } from "vue-toastification";
+  import axios from "axios";
+
+	const Toast = useToast();
+	const base_URI = import.meta.env.VITE_BASE_BACKEND_ENDPOINT;
+
+	const user = ref({
+		name: '',
+		email: '',
+		password: '',
+		passwordConfirmation: '',
+	})
+
+	const submit = () => {
+		if(user.value.password !== user.value.passwordConfirmation) {
+			return Toast.error('Passwords do not match');
+		}
+
+		console.log(`${base_URI}/auth`)
+
+		axios({
+			method: 'POST',
+			url: `${base_URI}/auth`,
+			data: user.value,
+		}).then(() => {
+			Toast.success('Successfully signed up');
+			router.push('/login');	
+		}).catch((e) => {
+			if (e.response.data)
+				Toast.error( e.response.data.message || "We struggling with goblins on out server 👺" );
+			else
+				Toast.error( "We struggling with goblins on out server 👺" );
+		});
+	}
+</script>
+
+<template>
+	<div class="mx-auto max-w-xs">
+		<form class="border-2 border-gray-600 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+
+			<!-- INPUT: name -->
+			<div class="mb-4">
+				<div class="flex-col space-y-2 justify-center">
+					<input class="w-11/12 focus:w-full bg-gray-600 shadow appearance-none border rounded py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline"
+						type="text"
+						placeholder="name"
+						v-model="user.name">
+					<input class="w-11/12 focus:w-full bg-gray-600 shadow appearance-none border rounded py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline"
+						type="text"
+						placeholder="E-mail"
+						v-model="user.email">
+					<input class="w-11/12 focus:w-full bg-gray-600 shadow appearance-none border rounded py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline"
+						type="password"
+						placeholder="Password"
+						v-model="user.password">
+					<input class="w-11/12 focus:w-full bg-gray-600 shadow appearance-none border rounded py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline"
+						type="password"
+						placeholder="Repeat password"
+						v-model="user.passwordConfirmation">
+				</div>
+			</div>
+
+			<!-- SUBMIT -->
+			<div class="flex items-center justify-center">
+				<button @click="submit" class="bg-blue-500 hover:bg-blue-700 text-dark font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+					Sign In
+				</button>
+			</div>
+		</form>
+	</div>
+
+
+</template>
