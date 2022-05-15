51744bdff0a201661c963210f83dd274e2d47955
Simon Hryszko <simon@hryszko.dev>
2022-05-15 17:03:22 +0200

    remved home old code (so CD will succed)

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,4 @@
-<script setup>
-	import { useCounterStore } from '../stores/token'
-
-			const counter = useCounterStore()
-
-			counter.count++
-			// with autocompletion ✨
-			counter.$patch({ count: counter.count + 1 })
-			// or using an action instead
-		counter.increment()
-</script>
-
 <template>
 	<div>
-		<!-- button -->
 	</div>
 </template>
