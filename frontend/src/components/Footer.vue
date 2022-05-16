df50d222f2b7235cb8cdd234bdc84de64ea6626b
Simon Hryszko <simon@hryszko.dev>
2022-05-16 19:47:55 +0200

    Made footer sticky

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,9 +1,11 @@
 <template>
-	<div>
-
-	<div class="bg-dark w-screen px-4 text-center mt-4">
-		&copy; 2022 - {{ new Date().getFullYear() }} | <a class="hover:text-blue-400" href="https://github.com/shirobachi">Shirobachi</a> made this with 🫀
-	</div>
+	<div class="fixed bottom-2 w-full">
+		<div class="flex justify-center">
+			<div class="bg-dark px-4 text-center mt-4 bg-slate-500 w-1/2 rounded-lg p-2">
+				&copy; 2022 - {{ new Date().getFullYear() }} | <a class="hover:text-blue-400" href="https://github.com/shirobachi">Shirobachi</a> made this with 🫀 in <a href="https://wakatime.com/badge/user/b74ba3c5-2883-43ca-9833-799f8a50840a/project/980bed0f-5bd3-466f-b332-8df76db58d54"><img src="https://wakatime.com/badge/user/b74ba3c5-2883-43ca-9833-799f8a50840a/project/980bed0f-5bd3-466f-b332-8df76db58d54.svg" alt="wakatime" class="inline-block"></a>
+			</div>
+			
+		</div>
 
 	</div>
 </template>
