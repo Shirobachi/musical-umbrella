98ee33584678c3107041526b332e84784d7ba02e
Simon Hryszko <simon@hryszko.dev>
2022-05-17 00:16:07 +0200

    Fixed link to repo

--- frontend/src/views/about.vue
+++ frontend/src/views/about.vue
@@ -5,7 +5,7 @@
 		Thanks for stepping by my site! This is site to manage offices premises. You can sign up and see some feature. <br>
 		BTW. I left some easter egg in the code. So have eye open and see what happens. <br><br>
 
-		This repo is open source. You can find it here: <a class="text-blue-400 hover:cursor-pointer hover:underline" href="https://github.com/Shirobachi/musical-umbrella" target="_blank">https://github.com/Shirobachi/musical-umbrella/projects?type=beta</a> <br>
+		This repo is open source. You can find it here: <a class="text-blue-400 hover:cursor-pointer hover:underline" href="https://github.com/Shirobachi/musical-umbrella" target="_blank">https://github.com/Shirobachi/musical-umbrella</a> <br>
 		Check out my github: <a class="text-blue-400 hover:cursor-pointer hover:underline" href="https://github.com/Shirobachi">https://github.com/Shirobachi</a> <br>
 		Or send me a mail: <a class="text-blue-400 hover:cursor-pointer hover:underline" href="mailto:simon@hryszko.dev">mailto:simon@hryszko.dev</a>
 
